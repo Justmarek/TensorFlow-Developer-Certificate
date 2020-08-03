29e17d4ceadde14ccd5a2a4754f0d0b640c4667c
marekbiernacki <MABiernacki@gmail.com>
2020-08-03 22:32:32 +0100

    image class update

--- Pycharm-Script-Practice/Workbooks/Image-Classification-Clean-Solution.py
+++ Pycharm-Script-Practice/Workbooks/Image-Classification-Clean-Solution.py
@@ -1,123 +1,49 @@
+#!/usr/bin/env python
+
+"""All that is required for the Image Classification component of the TensorFlow Developer Certificate"""
+
+__author__ = "Marek Biernacki"
+__date__ = "3 August 2020"
+
+
 # Import libraries
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-import os
-import numpy as np
 import matplotlib.pyplot as plt
-# Silence errors
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
+import numpy as np
+import os
+import PIL
+import pathlib
 
-# Load data
-_URL = 'https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip'
+# TensorFlow imports
+import tensorflow as tf
+from tensorflow import keras
 
-path_to_zip = tf.keras.utils.get_file('cats_and_dogs.zip', origin=_URL, extract=True)
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import preprocessing
 
-PATH = os.path.join(os.path.dirname(path_to_zip), 'cats_and_dogs_filtered')
+# Downloading data
+dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+data_dir = tf.keras.utils.get_file('flower_photos', origin=dataset_url, untar=True)
+data_dir = pathlib.Path(data_dir)
 
-# Create directories for train and test
-train_dir = os.path.join(PATH, 'train')
-validation_dir = os.path.join(PATH, 'validation')
+# Preset parameters
+batch_size = 32
+img_height = 180
+img_width = 180
 
-# Create directories to split the train and test dog sets
-train_cats_dir = os.path.join(train_dir, 'cats')  # directory with our training cat pictures
-train_dogs_dir = os.path.join(train_dir, 'dogs')  # directory with our training dog pictures
-validation_cats_dir = os.path.join(validation_dir, 'cats')  # directory with our validation cat pictures
-validation_dogs_dir = os.path.join(validation_dir, 'dogs')  # directory with our validation dog pictures
+# Data preprocessing
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
 
-# Understanding the data
-num_cats_tr = len(os.listdir(train_cats_dir))
-num_dogs_tr = len(os.listdir(train_dogs_dir))
-
-num_cats_val = len(os.listdir(validation_cats_dir))
-num_dogs_val = len(os.listdir(validation_dogs_dir))
-
-total_train = num_cats_tr + num_dogs_tr
-total_val = num_cats_val + num_dogs_val
-
-# Environmental variables
-batch_size = 128
-epochs = 15
-IMG_HEIGHT = 150
-IMG_WIDTH = 150
-
-# Data preparation - Create Image Generator class, it can read images from disk and prepare them
-train_image_generator = ImageDataGenerator(rescale=1./255,
-                                           rotation_range=45,
-                                           width_shift_range=.15,
-                                           height_shift_range=.15,
-                                           horizontal_flip=True,
-                                           zoom_range=0.5
-                                           )
-
-# Validation generator
-validation_image_generator = ImageDataGenerator(rescale=1. / 255)  # Generator for our validation set
-
-# After creating the generators for training and validation images, the flow_from_directo
-# Applies rescaling and resizes the images into the required dimensions
-train_data_gen = train_image_generator.flow_from_directory(batch_size=batch_size,
-                                                           directory=train_dir,
-                                                           shuffle=True,
-                                                           target_size=(IMG_HEIGHT, IMG_WIDTH),
-                                                           class_mode='binary')
-
-val_data_gen = validation_image_generator.flow_from_directory(batch_size=batch_size,
-                                                              directory=validation_dir,
-                                                              target_size=(IMG_HEIGHT, IMG_WIDTH),
-                                                              class_mode='binary')
-
-# Create the model, includes dropout for regularization
-model = Sequential([
-    Conv2D(16, 3, padding='same', activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH ,3)),
-    MaxPooling2D(),
-    Dropout(0.2),
-    Conv2D(32, 3, padding='same', activation='relu'),
-    MaxPooling2D(),
-    Dropout(0.2),
-    Conv2D(64, 3, padding='same', activation='relu'),
-    MaxPooling2D(),
-    Dropout(0.2),
-    Flatten(),
-    Dense(512, activation='relu'),
-    Dense(1)
-])
-
-# Compile the model
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-              metrics=['accuracy'])
-
-model.summary()
-
-# Train the model
-history = model.fit(
-    train_data_gen,
-    steps_per_epoch=total_train // batch_size,
-    epochs=epochs,
-    validation_data=val_data_gen,
-    validation_steps=total_val // batch_size
-)
-
-# Visualizing the results
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
-
-loss=history.history['loss']
-val_loss=history.history['val_loss']
-
-epochs_range = range(epochs)
-
-plt.figure(figsize=(8, 8))
-plt.subplot(1, 2, 1)
-plt.plot(epochs_range, acc, label='Training Accuracy')
-plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-plt.legend(loc='lower right')
-plt.title('Training and Validation Accuracy')
-
-plt.subplot(1, 2, 2)
-plt.plot(epochs_range, loss, label='Training Loss')
-plt.plot(epochs_range, val_loss, label='Validation Loss')
-plt.legend(loc='upper right')
-plt.title('Training and Validation Loss')
-plt.show()
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
