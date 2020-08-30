27d387b93fe51d4e5581c50d0bd5606d1bb24b4c
marekbiernacki <MABiernacki@gmail.com>
2020-08-30 21:26:01 +0100

    second workbook added

---
+++ 03-Tensorflow-in-Practice-Pycharm-Notebooks/Course 1 - Introduction To TensorFlow/Week2/Week2-Part3-Callbacks.py
@@ -0,0 +1,33 @@
+# Import libraries
+import tensorflow as tf
+
+# Create callbacks class
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if(logs.get("accuracy")>0.6):
+            print("\nReached 60% accuracy so cancelling training!")
+            self.model.strop_training = True
+
+# Import data
+mnist = tf.keras.datasets.fashion_mnist
+
+# Creat test train split
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# Instantiate callback
+callbacks = myCallback()
+
+# Creating the model
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28,28)),
+    tf.keras.layers.Dense(512, activation=tf.nn.relu),
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+# Compile model
+model.compile(optimizer=tf.optimizers.Adam(),
+             loss='sparse_categorical_crossentropy',
+             metrics=["accuracy"])
+
+# Fit model
+model.fit(x_train, y_train, epochs=1, callbacks=[callbacks])
---
+++ 03-Tensorflow-in-Practice-Pycharm-Notebooks/Course 1 - Introduction To TensorFlow/Week2/Week2-Part4-Handwriting-Recognition.py
@@ -0,0 +1,36 @@
+# import libraries
+import tensorflow as tf
+from os import path, getcwd, chdir
+
+path = f"{getcwd()}/data/mnist.npz"
+
+def train_mnist():
+    # Create callback class
+    class myCallback(tf.keras.callbacks.Callback):
+        def on_epoch_end(self, epoch, logs={}):
+            if(logs.get("accuracy")>0.99):
+                print("\nReached 99% accuracy so cancelling training!")
+                self.model.stop_training = True
+    # Instantiate callback
+    callbacks = myCallback()
+    # Load data
+    mnist = tf.keras.datasets.mnist
+    # Create train test split
+    (x_train, y_train), (x_test, y_test) = mnist.load_data(path)
+    # Normalise data
+    x_train = x_train/255.0
+    x_test = x_test/255.0
+    # Create model
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(512, activation=tf.nn.relu),
+        tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+    model.compile(optimizer="adam",
+                  loss="sparse_categorical_crossentropy",
+                  metrics=["accuracy"])
+    # model fitting
+    history = model.fit(x_train, y_train, epochs=10, callbacks=[callbacks])
+    return history.epoch, history.history["accuracy"][-1]
+
+# Run function
+train_mnist()
