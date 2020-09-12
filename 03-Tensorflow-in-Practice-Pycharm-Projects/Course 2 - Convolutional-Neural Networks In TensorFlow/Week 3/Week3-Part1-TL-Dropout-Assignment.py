dd36ac2fa7dbd58f163b80d5d0ad1ecdbed4d7bd
marekbiernacki <MABiernacki@gmail.com>
2020-09-12 20:38:59 +0100

    week 3 notebooks assignment added

--- 03-Tensorflow-in-Practice-Pycharm-Projects/Course 2 - Convolutional-Neural Networks In TensorFlow/Week 3/Week3-Part1-TL-Dropout-Assignment.py
+++ 03-Tensorflow-in-Practice-Pycharm-Projects/Course 2 - Convolutional-Neural Networks In TensorFlow/Week 3/Week3-Part1-TL-Dropout-Assignment.py
@@ -6,6 +6,8 @@ from tensorflow.keras import Model
 from tensorflow.keras.applications.inception_v3 import InceptionV3
 from os import getcwd
 from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from matplotlib import pyplot as plt
 
 # Import inception model
 path_inception = f"{getcwd()}/pretrainedmodel/inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5"
@@ -23,8 +25,6 @@ pre_trained_model.load_weights(local_weights_file)
 for layer in pre_trained_model.layers:
     layer.trainable = False
 
-pre_trained_model.summary()
-
 last_layer = pre_trained_model.get_layer('mixed7')
 print('last layer output shape:' , last_layer.output_shape)
 last_output = last_layer.output
@@ -37,7 +37,7 @@ class myCallback(tf.keras.callbacks.Callback):
             self.model.stop_training = True
 
 # Flatten the output layer to 1 dimension
-x = layers.FLatten()(last_output)
+x = layers.Flatten()(last_output)
 # Add a fully connected layer with 1,024 hidden units and ReLU activation
 x = layers.Dense(1024, activation="relu")(x)
 # Add a dropout rate of 0.2
@@ -45,5 +45,83 @@ x = layers.Dropout(0.2)(x)
 # Add a final sigmoid layer for classification
 x = layers.Dense(1, activation="sigmoid")(x)
 
+# Create model
+model = Model(pre_trained_model.input, x)
 
+# Compile model
+model.compile(optimiser = RMSprop(lr=0.0001),
+              loss="binary_crossentropy",
+              metrics=['accuracy'])
 
+model.summary()
+
+# Import data
+base_dir = "data/cats_and_dogs_filtered"
+
+# Train and validation directories
+train_dir = os.path.join(base_dir, "train")
+validation_dir = os.path.join(base_dir, "validation")
+
+train_cats_dir = os.path.join(train_dir, 'cats') # Directory with our training cat pictures
+train_dogs_dir = os.path.join(train_dir, 'dogs') # Directory with our training dog pictures
+validation_cats_dir = os.path.join(validation_dir, 'cats') # Directory with our validation cat pictures
+validation_dogs_dir = os.path.join(validation_dir, 'dogs') # Directory with our validation dog pictures
+train_cat_fnames = os.listdir(train_cats_dir)
+train_dog_fnames = os.listdir(train_dogs_dir)
+
+# Add our data-augmentation parameters to ImageDataGenerator
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                   rotation_range=40,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True)
+
+# Validation data should not be augmented
+test_datagen = ImageDataGenerator(rescale=1.0/255)
+
+# Flow training images in batches of 20 using train_datagen generator
+train_generator = train_datagen.flow_from_directory(train_dir,
+                                                    batch_size=20,
+                                                    class_mode="binary",
+                                                    target_size=(150, 150))
+
+# Flow validation images in batches od 20 using test_datagen generator
+validation_generator = test_datagen.flow_from_directory(validation_dir,
+                                                        batch_size=20,
+                                                        class_mode="binary",
+                                                        target_size=(150, 150))
+
+# Create callbacks class
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if(logs.get("accuracy")>0.80):
+            print("\nReached 98% accuracy so cancelling training!")
+            self.model.stop_training = True
+
+callbacks = myCallback()
+
+history = model.fit(
+            train_generator,
+            validation_data=validation_generator,
+            steps_per_epoch=100,
+            epochs=20,
+            validation_steps=50,
+            verbose=2,
+            callbacks=[callbacks])
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'r', label='Training accuracy')
+plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.legend(loc=0)
+plt.figure()
+
+plt.show()
