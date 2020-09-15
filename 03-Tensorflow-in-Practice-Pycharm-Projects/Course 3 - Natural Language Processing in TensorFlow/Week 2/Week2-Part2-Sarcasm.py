b3e2ae5392f05c6c87d5574795b3ed4cd9b27ff6
marekbiernacki <MABiernacki@gmail.com>
2020-09-16 00:03:02 +0100

    final commit for the evening continue tomorrow

--- 03-Tensorflow-in-Practice-Pycharm-Projects/Course 3 - Natural Language Processing in TensorFlow/Week 2/Week2-Part2-Sarcasm.py
+++ 03-Tensorflow-in-Practice-Pycharm-Projects/Course 3 - Natural Language Processing in TensorFlow/Week 2/Week2-Part2-Sarcasm.py
@@ -1,6 +1,8 @@
 # Import libraries
 import json
+import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
@@ -28,4 +30,35 @@ testing_sentences = sentences[training_size:]
 training_labels = labels[0:training_size]
 testing_labels = labels[training_size:]
 
+# Tokenizer
+tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+tokenizer.fit_on_texts(training_sentences)
 
+word_index = tokenizer.word_index
+
+training_sequences = tokenizer.texts_to_sequences(training_sentences)
+training_padded = pad_sequences(training_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+testing_sequences = tokenizer.texts_to_sequences(testing_sentences)
+testing_padded = pad_sequences(testing_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+training_padded = np.array(training_padded)
+training_labels = np.array(training_labels)
+testing_padded = np.array(testing_padded)
+testing_labels = np.array(testing_labels)
+
+# Build and compile model
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+    tf.keras.layers.GlobalAveragePooling1D(),
+    tf.keras.layers.Dense(24, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+
+# Model summary
+model.summary()
+
+# Fit model
+num_epochs = 30
+history = model.fit(training_padded, training_labels, epochs=num_epochs, validation_data=(testing_padded, testing_labels), verbose=2)
